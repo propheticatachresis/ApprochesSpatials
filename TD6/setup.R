46cccc73fc3571e9f87f97902487a89491f2f7a2
Jiaxun Cai <62858810+propheticatachresis@users.noreply.github.com>
2022-11-22 16:10:35 +0100

    Add files via upload

---
+++ TD6/setup.R
@@ -0,0 +1,2 @@
+options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"))
+install.packages(c('tidyverse', 'sf', 'mapview', 'xaringan', 'xaringanthemer', 'showtext', 'mapsf'))
